70853ca05a48bc2732d4000dd1e5bae88b7beb77
Shirakawa <673254896@qq.com>
2018-02-28 16:48:52 +0800

    空格筛选

--- add.py
+++ add.py
@@ -10,7 +10,12 @@ rs = redis.StrictRedis(host=redis_host,port=redis_port,db=redis_db)
 rs.delete('Serverlist')
 
 for i in range(1, len(sys.argv)):
-    print "URL", i, sys.argv[i]
-    print rs.sadd('Serverlist',sys.argv[i])
-    print 'default Code 200'
-    print rs.set(sys.argv[i],'200')
+    if sys.argv[i] == '':
+        print sys.argv[i]+' ,is null'
+    elif sys.argv[i] == ' ':
+        print sys.argv[i]+' ,is null'
+    else :
+        print "URL", i, sys.argv[i]
+        print rs.sadd('Serverlist',sys.argv[i])
+        print 'default Code 200'
+        print rs.set(sys.argv[i],'200')
